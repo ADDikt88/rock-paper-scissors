5d3d070405c448df268c7764ad5c35e6dde2bf1e
ADDikt88 <kyletsang8@gmail.com>
2024-08-12 21:13:42 -0400

    Rock paper scissors game with ui
    Player can now play in the browser and has 3
    working buttons for choice. First player to 5
    wins! Once a player or computer reaches 5, player
    must hit 'click to play again' button to reset the
    game.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+ROCK PAPER SCISSORS GAME
+
+This website is a classic game of rock paper scissors.
+A human competes against the computer and a score is
+tracked on the screen.
--- rps.js
+++ rps.js
@@ -12,11 +12,56 @@ function getHumanChoice() {
 }
 
 /*console.log(getHumanChoice());*/
-
 let humanScore = 0;
 let computerScore = 0;
 
+const container = document.querySelector("#container");
+const playAgainBtn = document.createElement("button");
+playAgainBtn.textContent = "Click to reset game";
+
+const humanChoiceText = document.querySelector("#human-choice");
+const compChoiceText= document.querySelector("#computer-choice");
+const startPhrase= document.querySelector("#start-phrase");
+
+function endGame() {
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            if (humanScore == 5) {
+                alert("Human Wins!");
+                startPhrase.textContent = "Human Won!";
+            }
+            else if (computerScore == 5) {
+                alert("Computer Wins!");
+                startPhrase.textContent = "Computer Won!";
+            }
+
+        });
+    });
+        
+    container.appendChild(playAgainBtn);
+    playAgainBtn.addEventListener ('click', () => {
+        resetGame();
+    });
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    humanChoiceText.textContent = "Human chooses: "
+    compChoiceText.textContent = "Computer chooses: "
+
+    scoreText.textContent = "Human Score: " + humanScore + " Computer Score: " + computerScore;
+    startPhrase.textContent = "First to 5 wins!";
+    container.removeChild(playAgainBtn);
+    
+}
+
+
 function playRound (humanChoice, computerChoice) {
+
+    humanChoiceText.textContent = "Human chooses: " + humanChoice;
+    compChoiceText.textContent = "Computer chooses: " + computerChoice;
 
     if (humanChoice.toLowerCase() == 'rock') {
         if (computerChoice == 'scissors') {
@@ -61,24 +106,51 @@ function playRound (humanChoice, computerChoice) {
     }
 }
 
+function playGame(humanSelection) {
 
+    //const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log (humanSelection + " vs. " + computerSelection);
+    playRound(humanSelection, computerSelection);
+    console.log ("Human Score: " + humanScore + " Computer Score: " + computerScore);
+    scoreText.textContent = "Human Score: " + humanScore + " Computer Score: " + computerScore;
 
-function playGame() {
-
-    let i = 0;
-
-    alert("Let's play 5 rounds of rock paper scissors");
-    while (i < 5)
-    {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log (humanSelection + " vs. " + computerSelection);
-        playRound(humanSelection, computerSelection);
-        
-        i++;
-            
+    if (humanScore == 5 || computerScore == 5) {
+        endGame();
     }
-    alert ("Final Score - Human: " + humanScore + " Computer: " + computerScore)
+         
+    //alert ("Final Score - Human: " + humanScore + " Computer: " + computerScore)
 }
 
-playGame();
+/******* This is the UI for the rps game *******/
+
+const scoreText = document.querySelector("#score");
+scoreText.textContent = "Human Score: " + humanScore + " Computer Score: " + computerScore;
+
+
+/* 3 buttons for each selection (rps) */
+const rockBtn = document.querySelector("#rock-button");
+const paperBtn = document.querySelector("#paper-button");
+const scissorBtn = document.querySelector("#scissor-button");
+
+rockBtn.addEventListener ('click', () => {
+    if (humanScore < 5 && computerScore < 5)
+    {
+        playGame("rock");
+    }
+    
+});
+paperBtn.addEventListener ('click', () => {
+    
+    if (humanScore < 5 && computerScore < 5)
+    {
+        playGame("paper");
+    }
+});
+scissorBtn.addEventListener ('click', () => {
+    
+    if (humanScore < 5 && computerScore < 5)
+    {
+        playGame("scissors");
+    }
+});
